eca4bca52e00a6644503054db08987d0585b0ad7
yahya09206 <yahyahussein91@gmail.com>
2025-01-06 12:19:21 -0800

    left comments for steps to be completed

--- src/test/java/com/yahya/steps/SpartanAPISteps.java
+++ src/test/java/com/yahya/steps/SpartanAPISteps.java
@@ -9,14 +9,19 @@ public class SpartanAPISteps {
     @Given("the base_uri and base_path set")
     public void the_base_uri_and_base_path_set() {
         System.out.println("base_uri");
+        // set up base uri and base path
+        // save it into variable and make it global
     }
     @When("I send request to {string} endpoint")
     public void i_send_request_to_endpoint(String endpoint) {
         System.out.println(endpoint);
+        // send your request and save the result into a variable and make it global
     }
     @Then("I should get status code {int}")
     public void i_should_get_status_code(Integer int1) {
         System.out.println(int1);
+
+        // make assertions and save it into class level variable if more validation is needed
     }
 
 }
