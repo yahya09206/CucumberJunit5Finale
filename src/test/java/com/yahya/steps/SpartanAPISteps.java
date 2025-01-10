f8f3af09128721b3ddbb4add660fd047a55cadbe
yahya09206 <yahyahussein91@gmail.com>
2025-01-10 09:20:41 -0800

    setting up response

--- src/test/java/com/yahya/steps/SpartanAPISteps.java
+++ src/test/java/com/yahya/steps/SpartanAPISteps.java
@@ -3,6 +3,7 @@ package com.yahya.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
@@ -10,7 +11,8 @@ import static org.hamcrest.Matchers.*;
 
 public class SpartanAPISteps {
 
-    RequestSpecification givenPart
+    RequestSpecification givenPart;
+    Response response;
 
     @Given("the base_uri and base_path set")
     public void the_base_uri_and_base_path_set() {
@@ -26,6 +28,7 @@ public class SpartanAPISteps {
     public void i_send_request_to_endpoint(String endpoint) {
         System.out.println(endpoint);
         // send your request and save the result into a variable and make it global
+        response = givenPart.when().get(endpoint).prettyPeek();
     }
     @Then("I should get status code {int}")
     public void i_should_get_status_code(Integer statusCode) {
