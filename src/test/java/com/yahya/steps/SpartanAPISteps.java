5980f414fa7fe99aae7adc9ca596221631b91308
yahya09206 <yahyahussein91@gmail.com>
2025-01-10 09:18:50 -0800

    setting up given

--- src/test/java/com/yahya/steps/SpartanAPISteps.java
+++ src/test/java/com/yahya/steps/SpartanAPISteps.java
@@ -3,14 +3,24 @@ package com.yahya.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class SpartanAPISteps {
+
+    RequestSpecification givenPart
 
     @Given("the base_uri and base_path set")
     public void the_base_uri_and_base_path_set() {
         System.out.println("base_uri");
         // set up base uri and base path
         // save it into variable and make it global
+        givenPart = given().log().all().baseUri("http://44.211.192.252:8000")
+                .basePath("/api");
+
+
     }
     @When("I send request to {string} endpoint")
     public void i_send_request_to_endpoint(String endpoint) {
