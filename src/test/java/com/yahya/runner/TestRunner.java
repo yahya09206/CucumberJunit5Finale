050a10f2fcd7c3b064e6c27d02839705c6e3c1f3
yahya09206 <yahyahussein91@gmail.com>
2025-01-13 13:07:57 -0800

    adding tags to runner class

--- src/test/java/com/yahya/runner/TestRunner.java
+++ src/test/java/com/yahya/runner/TestRunner.java
@@ -1,5 +1,18 @@
 package com.yahya.runner;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("resources/rest")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.yahya.steps")
 public class TestRunner {
+
+
+
 }
